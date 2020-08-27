240a9324bed4e0dde23e81ad426f3e7a87001b0c
Kazik Pogoda <morisil@xemantic.com>
2020-08-27 23:19:26 +0200

    facebook pixel moved to custom.js

--- custom.js
+++ custom.js
@@ -12,6 +12,17 @@ gtag("config", GA_ID);
 
 addScript(GA_DIST);
 
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2406291223000018');
+fbq('track', 'PageView');
+
 // TODO remove invalid GA
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
